016b901549fc3e3549606b5a8434d67ba72a80cd
Chi-Wei Lien <wilsonwilson3124@gmail.com>
2022-09-16 13:40:52 -0400

    add landing image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // 'hero-img': "linear-gradient(to right bottom, rgba(43, 108, 176, 0.9), rgba(43, 108, 176, 0.9)), url('/images/cover.png')"
+        'hero-img': "radial-gradient(circle, rgba(247,246,157,0.006039915966386533) 22%, rgba(18,18,18,0.6474964985994398) 60%), url('/images/cover.png')"
+      }
+    },
   },
   plugins: [],
 }
